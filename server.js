da276ba33cc2a1bad78905e82ae1bbe3a148971b
Rellyson <rellysondouglas2015@gmail.com>
2020-07-03 11:55:21 -0300

    Added: local data to create a teacher

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
+server.use(routes)
 
 server.set('view engine', 'njk')
 
-server.use(routes)
 
 nunjucks.configure('views', {
     express: server,
