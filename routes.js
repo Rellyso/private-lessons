da80ebe409b71678cf717c8b4f48f6a373408b08
Rellyson <rellysondouglas2015@gmail.com>
2020-07-13 11:19:12 -0300

    Added student routes | refraathing routes sequence

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const teachers = require('./teachers')
+const teachers = require('./controllers/teachers')
+const students = require('./controllers/students')
 const routes = express.Router()
 
 routes.get('/', function (req, res) {
@@ -7,20 +8,20 @@ routes.get('/', function (req, res) {
 })
 
 routes.get('/teachers', teachers.index)
-
 routes.post('/teachers', teachers.post)
-
+routes.get('/teachers/create', teachers.create)
+routes.get('/teachers/:id', teachers.show)
+routes.get('/teachers/:id/edit', teachers.edit)
 routes.put('/teachers', teachers.put)
-
 routes.delete('/teachers', teachers.delete)
 
-routes.get('/teachers/create', function (req, res) {
-    return res.render('teachers/create')
-})
-
-routes.get('/teachers/:id', teachers.show)
-
-routes.get('/teachers/:id/edit', teachers.edit)
+routes.get('/students', students.index)
+routes.post('/students', students.post)
+routes.get('/students/create', students.create)
+routes.get('/students/:id', students.show)
+routes.get('/students/:id/edit', students.edit)
+routes.put('/students', students.put)
+routes.delete('/students', students.delete)
 
 
 module.exports = routes
