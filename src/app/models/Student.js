55271c0acc6414dd652fd73d567857721ca6181c
Rellyson <rellysondouglas2015@gmail.com>
2020-08-11 10:01:11 -0300

    Adding paginate query to return paginate function on index page

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -94,5 +94,42 @@ module.exports = {
             
             callback(results.rows)
         })
+    },
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+
+        let query = '',
+            filterQuery = '',
+            totalQuery = `(
+                SELECT count(*) FROM students
+                ) AS total
+            `
+
+        if (filter) {
+            filterQuery = `
+                WHERE students.name ILIKE '%${filter}%'
+                OR students.email ILIKE '%${filter}%'
+            `
+
+            totalQuery = `(
+                SELECT count(*) FROM students
+                ${filterQuery}
+                ) AS total
+            `
+        }
+
+        query = `
+            SELECT students.*, ${totalQuery}, count(students) AS total_students
+            FROM students
+            ${filterQuery}
+            GROUP BY students.id LIMIT $1 OFFSET $2
+
+        `
+
+        db.query(query, [limit, offset], (err, results) => {
+            if (err) throw `Database Error! ${err}`
+
+            callback(results.rows)
+        })
     }
 }
