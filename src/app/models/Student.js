a8c72585be9be5fbd5f6653ef130ed7f687a87fc
Rellyson <rellysondouglas2015@gmail.com>
2020-12-23 14:04:31 -0300

    Updated to adapt async await functions and Base

--- src/app/models/Student.js
+++ src/app/models/Student.js
@@ -1,135 +1,24 @@
+const Base = require('./Base')
 const db = require('../../config/db')
-const { date } = require('../../lib/utils')
 
-module.exports = {
-    all(callback) {
-        db.query(`
-        SELECT * FROM students
-        ORDER BY name ASC
-        `, function (err, results) {
-            if (err) throw `Database error!! ${err}`
-            
-            callback(results.rows)
-        })
+Base.init({ table: 'students' })
+
+const Student = {
+    ...Base,
+    async selectTeacherOptions() {
+        const results = await db.query(`SELECT name, id FROM teachers`)
+
+        return results.rows
     },
-    create(data, callback) {
-        const query = `
-        INSERT INTO students (
-            name,
-            avatar_url,
-            email,
-            birth_date,
-            school_level,
-            workload,
-            teacher_id
-        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-        RETURNING id
-        `
-        const values = [
-            data.name,
-            data.avatar_url,
-            data.email,
-            date(data.birth_date).iso,
-            data.school_level,
-            data.workload,
-            data.teacher_id
-        ]
+    async findWithTeacher(id) {
+        let query = `SELECT students.*, teachers.name AS teacher_name FROM students
+            LEFT JOIN teachers ON (students.teacher_id = teachers.id)
+            WHERE students.id = $1`
 
-        db.query(query, values, function (err, results) {
-            if (err) throw `Database Error!! ${err}`
+        const results = await db.query(query, [id])
 
-            callback(results.rows[0].id)
-        })
-    },
-    find(id, callback) {
-        db.query(`
-        SELECT * FROM students
-        WHERE id = $1
-        `, [id], function (err, results) {
-            if (err) throw `Database Error!! ${err}`
-
-            callback(results.rows[0])
-        })
-    },
-    update(data, callback) {
-        const query = `
-        UPDATE students SET
-        name=($1),
-        avatar_url=($2),
-        email=($3),
-        birth_date=($4),
-        school_level=($5),
-        workload=($6),
-        teacher_id=($7)
-        WHERE id=$8
-        `
-
-        const values = [
-            data.name,
-            data.avatar_url,
-            data.email,
-            date(data.birth_date).iso,
-            data.school_level,
-            data.workload,
-            data.teacher_id,
-            data.id
-        ]
-
-        db.query(query, values, function (err, results) {
-            if (err) throw `Database Error!! ${err}`
-
-            callback()
-        })
-    },
-    delete(id, callback) {
-        db.query(`DELETE FROM students WHERE id = $1`, [id], function(err, results) {
-            if (err) throw `Database Error! ${err}`
-
-            callback()
-        })
-    },
-    selectTeacherOptions(callback) {
-        db.query(`SELECT name, id FROM teachers`, function (err, results) {
-            if (err) throw `Database Error!! ${err}`
-            
-            callback(results.rows)
-        })
-    },
-    paginate(params) {
-        const { filter, limit, offset, callback } = params
-
-        let query = '',
-            filterQuery = '',
-            totalQuery = `(
-                SELECT count(*) FROM students
-                ) AS total
-            `
-
-        if (filter) {
-            filterQuery = `
-                WHERE students.name ILIKE '%${filter}%'
-                OR students.email ILIKE '%${filter}%'
-            `
-
-            totalQuery = `(
-                SELECT count(*) FROM students
-                ${filterQuery}
-                ) AS total
-            `
-        }
-
-        query = `
-            SELECT students.*, ${totalQuery}, count(students) AS total_students
-            FROM students
-            ${filterQuery}
-            GROUP BY students.id LIMIT $1 OFFSET $2
-
-        `
-
-        db.query(query, [limit, offset], (err, results) => {
-            if (err) throw `Database Error! ${err}`
-
-            callback(results.rows)
-        })
+        return results.rows[0]
     }
 }
+
+module.exports = Student
