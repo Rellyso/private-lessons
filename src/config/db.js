efbc4dd4cf1212320c09e47a7b7ed2b8a98861d0
rellyso <rellysondouglas2015@gmail.com>
2021-01-18 15:08:56 -0300

    reconfiguring database

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
     user: 'postgres',
-    password: 'rdtc04042001',
+    password: 'postgres',
     host: 'localhost',
     port: 5432,
     database: 'my_teacher'
