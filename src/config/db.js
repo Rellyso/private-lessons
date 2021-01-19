a2acf85781925f1ddb5e18cd81ac3e59d44e7958
rellyso <rellysondouglas2015@gmail.com>
2021-01-19 09:07:17 -0300

    getting security at database user and password

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg')
+require('dotenv/config')
 
 module.exports = new Pool({
-    user: 'postgres',
-    password: 'postgres',
+    user: process.env.LOCAL_DATABASE_USER,
+    password: process.env.LOCAL_DATABASE_PASSWORD,
     host: 'localhost',
     port: 5432,
     database: 'my_teacher'
